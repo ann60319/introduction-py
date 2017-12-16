52a279d680165e87bd38902f26809d45d9086b35
ann60319 <34393480+ann60319@users.noreply.github.com>
2017-12-16 20:01:34 +0800

    Update ex_07_02_2.py

--- ex_07_02_2.py
+++ ex_07_02_2.py
@@ -13,3 +13,4 @@ print("Average spam confidence:",ans)
 ＃Desired Output
 ＃Average spam confidence: 0.750718518519
 ＃Average spam confidence: 0.7507185185185187 mine ave
+# http://www.py4e.com/code3/mbox-short.txt
