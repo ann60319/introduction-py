69a5c3f22abf86db5bbf92bbe573de69428d5a95
ann60319 <34393480+ann60319@users.noreply.github.com>
2017-12-17 23:59:16 +0800

    Add files via upload

---
+++ ex_08_02.py
@@ -0,0 +1,13 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+for line in fh:
+	lst=line.split()
+	if len(lst)>＝2 and lst[0]=='From':
+		count=count+1
+		print(line[2])
+	else:
+		continue
+print("There were", count, "lines in the file with From as the first word")
